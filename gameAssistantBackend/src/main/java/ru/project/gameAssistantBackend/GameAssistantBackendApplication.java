42935c7c360a8051b219465bf61bfd9aab17b16e
mihail <sapegin2005@yandex.ru>
2025-09-11 20:24:10 +0500

    first commit

---
+++ gameAssistantBackend/src/main/java/ru/project/gameAssistantBackend/GameAssistantBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.project.gameAssistantBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameAssistantBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameAssistantBackendApplication.class, args);
+	}
+
+}
---
+++ gameAssistantBackend/src/test/java/ru/project/gameAssistantBackend/GameAssistantBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.project.gameAssistantBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameAssistantBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
