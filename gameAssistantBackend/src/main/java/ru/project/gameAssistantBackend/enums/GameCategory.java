ae34cc48137232e5e983a359efbb1ed0baf2973d
Sapegin Mihail <sapegin2005@yandex.ru>
2025-11-13 20:31:51 +0500

    Поменял названия категорий игр

--- gameAssistantBackend/src/main/java/ru/project/gameAssistantBackend/enums/GameCategory.java
+++ gameAssistantBackend/src/main/java/ru/project/gameAssistantBackend/enums/GameCategory.java
@@ -1,6 +1,17 @@
 package ru.project.gameAssistantBackend.enums;
 
 public enum GameCategory {
-    FIRST,
-    SECOND
+    EUROGAME,
+    AMERITRASH,
+    CONTROL_OF_TERRITORIES,
+    WARGAME,
+    ABSTRACT,
+    FILLER,
+    ROLE,
+    CLASSIC_BOARD,
+    BRAINTEASER,
+    FAMILY,
+    CARD,
+    COOPERATIVE,
+    COLLECTION_CARD
 }
